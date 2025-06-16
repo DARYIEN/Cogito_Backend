f4ac9942e1e2fe8b5073c206d0d63aa11c2c9148
Niktia <seekergame123@gmail.com>
2025-06-16 23:12:53 +0700

    Application

---
+++ src/main/java/com/example/cogito_backend/CogitoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cogito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogitoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CogitoBackendApplication.class, args);
+    }
+
+}
